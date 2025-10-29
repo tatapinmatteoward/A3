4f61ae6b578eddfb7ff0bd57a7a6ca7b3c9db401
tatapinmatteoward <athcdejd81rul5@hotmail.com>
2025-10-29 11:09:57 +0000

    Add .vitepress/theme/posts.data.js

---
+++ .vitepress/theme/posts.data.js
@@ -0,0 +1,45 @@
+export default {
+  load() {
+    return [
+    {
+        "url": "/blog/brewing-restful-nights-herbal-teas-combat-insomnia-naturally",
+        "title": "Brewing Restful Nights: Herbal Teas to Combat Insomnia Naturally",
+        "excerpt": "Discover the soothing power of herbal teas that promote deep sleep and banish insomnia. Learn about chamomile, valerian root, and more for natural, restorative rest at Nature's Remedy Hub.",
+        "date": "2025-10-29",
+        "tags": [
+            "herbal teas",
+            "insomnia relief",
+            "natural sleep aids",
+            "wellness",
+            "chamomile"
+        ]
+    },
+    {
+        "url": "/blog/essential-oils-revolution-soothe-your-digestive-woes-at-home",
+        "title": "Essential Oils Revolution: Soothe Your Digestive Woes at Home",
+        "excerpt": "Discover how essential oils can naturally ease digestive issues like bloating and indigestion, all from the comfort of your home with Nature's Remedy Hub.",
+        "date": "2023-10-01",
+        "tags": [
+            "essential-oils",
+            "digestion",
+            "natural-remedies",
+            "aromatherapy",
+            "home-healing"
+        ]
+    },
+    {
+        "url": "/blog/plant-powered-relief-banishing-migraines-with-natures-helpers",
+        "title": "Plant-Powered Relief: Banishing Migraines with Nature's Helpers",
+        "excerpt": "Discover natural ways to combat migraines using potent plants and herbs. From peppermint to ginger, learn how nature's remedies can provide soothing relief for headache sufferers.",
+        "date": "2023-10-01",
+        "tags": [
+            "migraine",
+            "natural remedies",
+            "herbal medicine",
+            "plant-based relief",
+            "headache solutions"
+        ]
+    }
+]
+  }
+}
